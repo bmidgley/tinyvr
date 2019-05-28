9062d4c9d26dfcc62b26c103fcabed11737a9f0c
Brad Midgley <bmidgley@gmail.com>
2019-05-27 21:07:59 -0600

    use only the beta value to drive the servos

--- driver.py
+++ driver.py
@@ -3,6 +3,8 @@
 import servo
 import sys
 import re
+import math
+import time
 
 left_pin = 23
 right_pin = 24
@@ -10,24 +12,22 @@ right_pin = 24
 right = servo.Servo(left_pin, invert=True)
 left = servo.Servo(right_pin)
 
-def drive(pitch, roll):
-    # positive pitch for forward; positive roll for right
-    rs = pitch - roll
-    ls = pitch + roll
+def drive(a, direction, g):
+    rs = math.cos(direction+45)
+    ls = math.cos(direction-45)
+    print('rs %d ls %d' % (rs, ls))
     right.setSpeed(rs)
     left.setSpeed(ls)
-
-def drive_orientation(a, b, g):
-    pitch = ((g + 90) if (g < 0) else (g - 90)) / 90
-    roll = b / 90
-    drive(pitch, roll)
+    time.sleep(0.15)
+    left.setSpeed(0)
+    right.setSpeed(0)
 
 try:
     for line in sys.stdin:
         m = re.match(r'a=([-]?\d+)&b=([-]?\d+)&g=([-]?\d+)', line)
         if (m):
             v = m.groups()
-            drive_orientation(int(v[0]), int(v[1]), int(v[2])) 
+            drive(int(v[0]), int(v[1]), int(v[2]))
 except:
     left.stop()
     right.stop()
